d4df4bd204019a53ce69e459c8e7e23bcfd79524
Giovanni Ciandrini <giovanni.ciandrini@ximplia.it>
2022-01-31 10:09:45 +0100

    DataGateway setup

---
+++ datagat/EFDataGateway/EFDataGateway/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) BKN301 SPA 2021.
+// All Rights Reserved.
+using EFDataGateway;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EFDataGateway
+{
+
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> All();
+        Task<T> GetById(int id);
+        Task<bool> Add(T entity);
+        Task<bool> Delete(T entity);
+        Task<bool> Upsert(T entity);
+        Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
+
+    }
+
+    public class GenericRepository<T,Tcontext> : IGenericRepository<T> where T : class 
+        where Tcontext : DbContext
+    {
+
+        public readonly IUnitOfWork<Tcontext> _uow;
+
+        public GenericRepository(IUnitOfWork<Tcontext> uow)
+        {
+            _uow = uow;
+        }
+
+        public virtual async Task<bool> Add(T entity)
+        {
+            await _uow.Context.AddAsync(entity);
+            return true;
+        }
+
+        public virtual async Task<bool> AddAndGetId(T entity)
+        {
+            await _uow.Context.AddAsync(entity);
+            return true;
+        }
+
+        public virtual async Task<IEnumerable<T>> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<bool> Delete(T entity)
+        {
+            _uow.Context.Remove(entity);
+            return true;
+        }
+
+        public virtual async Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate)
+        {
+            return await _uow.Context.Set<T>().Where(predicate).ToListAsync();
+
+        }
+
+        public virtual async Task<T> GetById(int id)
+        {
+            return await _uow.Context.Set<T>().FindAsync(id);
+        }
+
+        public virtual async Task<bool> Upsert(T entity)
+        {
+            _uow.Context.Set<T>().Update(entity);
+            return true;
+        }
+    }
+
+}
---
+++ datagat/EFDataGateway/EFDataGateway/IUnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace EFDataGateway
+{
+    public interface IUnitOfWork<T> : IDisposable where T: DbContext
+    {
+        Task<int> CompleteAsync();
+        Task BeginTransaction();
+        void Dispose();
+        void Commit();
+        void Rollback();
+        T Context { get; }
+
+    }
+
+    public class UnitOfWork<T>: IUnitOfWork<T>, IDisposable where T: DbContext
+    {
+        public T Context { get; }
+        private IDbContextTransaction Transaction { get; set; }
+      
+
+        public UnitOfWork(T context)
+        {
+            Context = context;
+        }
+        public async Task BeginTransaction()
+        {
+            var transaction = await Context.Database.BeginTransactionAsync();
+            Transaction = transaction;
+        }
+
+        public void Commit()
+        {
+            Transaction.Commit();
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void Rollback()
+        {
+            Transaction.Rollback();
+        }
+    }
+}
